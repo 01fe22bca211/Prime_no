b720c925c45be9cad8976960c5d5d9922c111ef8
Vaishali.P <01fe22bca211@kletech.ac.in>
2024-03-05 20:42:54 +0530

    code related to prime number

---
+++ Prime_no.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Prime_no {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the starting number of the range: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the ending number of the range: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Prime numbers in the range " + start + " to " + end + ":");
+        PrimesInRange(start, end);
+
+        scanner.close();
+    }
+
+    private static void PrimesInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
+    }
+
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+        return true;   
+    }
+}    
